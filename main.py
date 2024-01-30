668bc45f1cd445ac762fc5a88a320890fdd49769
Oscar Ortega <odortega@gmail.com>
2024-01-30 18:42:24 +1000

    (add) flask session vars

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from flask import Flask, request, make_response, redirect,  render_template
+from flask import Flask, request, make_response, redirect,  render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
 todos = ['Buy coffee', 'Send request', 'Drink coffee']
+
+
+app.config.update(
+    DEBUG=False,
+    ENV='development'
+)
+
+# next line sets app config secret key
+app.config['SECRET_KEY'] = 'SUPER SECRET'
+
+
 
 # error handler function for 404 error
 @app.errorhandler(404)
@@ -21,12 +33,14 @@ def server_error(error):
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    #response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    #user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': todos
